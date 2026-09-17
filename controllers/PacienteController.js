42999800b21dd4192413041e7f503f055f5a4905
Natalin <natalin.inchauspe.web@gmail.com>
2026-09-17 18:56:30 +0200

    Agrego HTML y CSS para la página principal

---
+++ controllers/PacienteController.js
@@ -0,0 +1,29 @@
+import Paciente from "../models/PacienteModel.js";
+
+class PacienteController {
+    // Obtener todos los pacientes
+    async getAll(req, res) {
+        try {
+            const pacientes = await Paciente.find();
+            res.json({ message: 'success', data: pacientes });
+        } catch (error) {
+            res.status(500).json({ message: 'Error al obtener pacientes.' });
+        }
+    }
+
+    // Crear un paciente
+    async create(req, res) {
+        try {
+            const { nombre, dni, obraSocial, telefono } = req.body;
+            if (!nombre || !dni || !obraSocial || !telefono) {
+                return res.status(400).send("Faltan datos obligatorios.");
+            }
+            const nuevoPaciente = await Paciente.create({ nombre, dni, obraSocial, telefono });
+            res.status(201).json({ message: 'success', data: nuevoPaciente });
+        } catch (error) {
+            res.status(500).json({ message: 'Error al crear el paciente.' });
+        }
+    }
+}
+
+export default PacienteController;
